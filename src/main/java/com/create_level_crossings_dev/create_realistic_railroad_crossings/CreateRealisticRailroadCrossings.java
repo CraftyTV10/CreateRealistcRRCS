2dbb33c69ab7ee0687d05261cb9ec56106a1e20b
Footdablit2310 <kolayparamita@gmail.com>
2026-09-14 19:29:32 +0200

    Initial setup

---
+++ src/main/java/com/create_level_crossings_dev/create_realistic_railroad_crossings/CreateRealisticRailroadCrossings.java
@@ -0,0 +1,14 @@
+package com.create_level_crossings_dev.create_realistic_railroad_crossings;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.fml.common.Mod;
+
+
+@Mod(CreateRealisticRailroadCrossings.MODID)
+public class CreateRealisticRailroadCrossings {
+    public static final String MODID = "create_realistic_railroad_crossings";
+    public static final Logger LOGGER = LogUtils.getLogger();
+}
